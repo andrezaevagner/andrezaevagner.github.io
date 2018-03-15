c2d89213d4e4982144499f6e880caf6fa8bde029
Vagner Clementino <vagner.clementino@gmail.com>
2018-03-15 10:01:52 -0300

    Alterado a parte de contagem de dias do site

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,18 +51,18 @@ $(document).ready(function() {
 
         // find the amount of "seconds" between now and target
         var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
+        var seconds_left = ( Math.abs(target_date - current_date))  / 1000;
 
         // do some time calculations
         days = parseInt(seconds_left / 86400);
         seconds_left = seconds_left % 86400;
-         
+
         hours = parseInt(seconds_left / 3600);
         seconds_left = seconds_left % 3600;
-         
+
         minutes = parseInt(seconds_left / 60);
         seconds = parseInt(seconds_left % 60);
-         
+
         // format countdown string + set tag value.
         days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit script">Dias</span>';
         hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit script">Hrs</span>';
